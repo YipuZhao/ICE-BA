41bdb631d4e03cb29490c2ada368be8604587c2a
yipuzhao <yzhao347@gatech.edu>
2019-03-04 15:55:57 -0500

    freeze for now; add additional benchmarking script later

--- scripts/run_ice_ba_stereo.sh
+++ scripts/run_ice_ba_stereo.sh
@@ -8,6 +8,6 @@ Sequence=V2_03_difficult
 
 mkdir $EuRoC_PATH/result
 
-cmd="../bin/ice_ba --imgs_folder $EuRoC_PATH/$Sequence --start_idx 0 --end_idx -1 --iba_param_path ../config/config_of_stereo.txt  --gba_camera_save_path $EuRoC_PATH/result/$Sequence.txt --stereo --save_feature"
+cmd="../bin/ice_ba --imgs_folder $EuRoC_PATH/$Sequence --start_idx 0 --end_idx -1 --iba_param_path ../config/config_of_stereo_v2.txt --stereo" # --gba_camera_save_path $EuRoC_PATH/result/$Sequence.txt --save_feature
 echo $cmd
 eval $cmd
